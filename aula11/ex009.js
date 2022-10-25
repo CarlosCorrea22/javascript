4d15f880b66710656e4c533ec89af85a102b9f79
Carlos Eduardo <carlosrrea44@gmail.com>
2022-10-25 18:20:04 -0300

    exercicios de IF

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'holanda'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil' || pais == 'brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
